b07ffe51b503eff711724e2c169833a04e5b47d3
mamyh <likhmaon@gmail.com>
2022-10-26 06:50:23 +0600

    http url string_decoder buffer exploring

---
+++ index.js
@@ -0,0 +1,26 @@
+// dependencies
+const http = require('http');
+const { handleReqRes } = require('./helpers/handleReqRes');
+// app object - Module scaffolding
+const app = {};
+
+// configuration
+app.config = {
+    port: 3000,
+};
+
+// creating  server
+
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+
+    server.listen(app.config.port, () => {
+        console.log(`Listening to the port ${app.config.port}`);
+    });
+};
+
+// handling Request and Response
+app.handleReqRes = handleReqRes;
+
+// start server
+app.createServer();
