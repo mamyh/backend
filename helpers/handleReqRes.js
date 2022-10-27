ebc25ca91ac7260f43567a46169f8197c4ad26f7
mamyh <likhmaon@gmail.com>
2022-10-27 17:43:07 +0600

    writing process has been started

--- helpers/handleReqRes.js
+++ helpers/handleReqRes.js
@@ -1,50 +1,53 @@
 // dependencies
-const url = require('url');
-const { StringDecoder } = require('string_decoder');
-const routes = require('../routes');
-const { notFoundHandler } = require('../handlers/routeHandlers/notFoundHandler');
+const url = require("url");
+const { StringDecoder } = require("string_decoder");
+const routes = require("../routes");
+const {
+  notFoundHandler,
+} = require("../handlers/routeHandlers/notFoundHandler");
 // handler Object - Module scaffolding
 const handler = {};
 
 handler.handleReqRes = (req, res) => {
-    // request handling
-    const parsedUrl = url.parse(req.url, true);
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/|\/$/g, '');
-    const method = req.method.toLowerCase();
-    const queryStringObject = parsedUrl.query;
-    const headersObject = req.headers;
-    const decoder = new StringDecoder('utf-8');
-    const requestObject = {
-        parsedUrl,
-        path,
-        trimmedPath,
-        method,
-        queryStringObject,
-        headersObject,
-    };
-    const chosenRoutes = routes[trimmedPath] ? routes[trimmedPath] : notFoundHandler;
-    let realData = '';
-    chosenRoutes(requestObject, (statusCode, payload) => {
-        const myStatusCode = typeof statusCode === 'number' ? statusCode : 500;
-        const payloadObject = typeof payload === 'object' ? payload : {};
+  // request handling
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/|\/$/g, "");
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headersObject = req.headers;
+  const decoder = new StringDecoder("utf-8");
+  const requestObject = {
+    parsedUrl,
+    path,
+    trimmedPath,
+    method,
+    queryStringObject,
+    headersObject,
+  };
+  const chosenHandler = routes[trimmedPath]
+    ? routes[trimmedPath]
+    : notFoundHandler;
+  let realData = "";
 
-        const payloadStringify = JSON.stringify(payloadObject);
+  req.on("data", (buffer) => {
+    realData += decoder.write(buffer);
+  });
 
-        // hadle response
-        res.writeHead(myStatusCode);
-        res.end(payloadStringify);
-    });
-    req.on('data', (buffer) => {
-        realData += decoder.write(buffer);
-    });
+  req.on("end", () => {
+    realData += decoder.end();
+    chosenHandler(requestObject, (statusCode, payload) => {
+      const myStatusCode = typeof statusCode === "number" ? statusCode : 500;
+      const payloadObject = typeof payload === "object" ? payload : {};
 
-    req.on('end', () => {
-        realData += decoder.end();
-        console.log(realData);
-        // handle response
-        res.end('hello World ! How are you ??');
+      const payloadStringify = JSON.stringify(payloadObject);
+
+      // hadle response
+      res.writeHead(myStatusCode);
+      res.end(payloadStringify);
     });
+    res.end("hello World ! How are you ??");
+  });
 };
 
 module.exports = handler;
--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 // dependencies
 const http = require("http");
 const { handleReqRes } = require("./helpers/handleReqRes");
+const environments = require("./helpers/environments");
 
 // app object - Module scaffolding
 const app = {};
-
-// configuration
-app.config = {
-  port: 3000,
-};
 
 // creating  server
 
 app.createServer = () => {
   const server = http.createServer(app.handleReqRes);
 
-  server.listen(app.config.port, () => {
-    console.log(`Listening to the port ${app.config.port}`);
+  server.listen(environments.port, () => {
+    console.log(`Listening to the port ${environments.port}`);
   });
 };
 
