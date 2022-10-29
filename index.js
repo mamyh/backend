913d2d81d83217c8b9a40647ba9c8f48aef654c1
mamyh <likhmaon@gmail.com>
2022-10-30 00:00:33 +0600

    crud in file

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 // dependencies
-const http = require("http");
-const { handleReqRes } = require("./helpers/handleReqRes");
-const environments = require("./helpers/environments");
-
+const http = require('http');
+const { handleReqRes } = require('./helpers/handleReqRes');
+const environments = require('./helpers/environments');
+const lib = require('./lib/lib');
 // app object - Module scaffolding
 const app = {};
 
+// testing file system
+lib.remove('test', 'newFile', (err) => {
+    console.log(err);
+});
 // creating  server
-
 app.createServer = () => {
-  const server = http.createServer(app.handleReqRes);
+    const server = http.createServer(app.handleReqRes);
 
-  server.listen(environments.port, () => {
-    console.log(`Listening to the port ${environments.port}`);
-  });
+    server.listen(environments.port, () => {
+        console.log(`Listening to the port ${environments.port}`);
+    });
 };
 
 // handling Request and Response
